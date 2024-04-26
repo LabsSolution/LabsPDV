45576495b1ab28230ae644bc2cf59a7fcadba617
yTpVirus <igordossantos.00@hotmail.com>
2024-04-26 10:59:44 -0300

    Começo da implementação do Blazor

--- Properties/Resources.Designer.cs
+++ Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace Labs.Properties {
+namespace LabsBlazor.Properties {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace Labs.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Labs.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("LabsBlazor.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
