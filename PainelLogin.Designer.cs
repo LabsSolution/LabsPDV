b11fd18109177ea857737b3dc72dc37c8c62781d
yTpVirus <igordossantos.00@hotmail.com>
2024-04-26 16:39:19 -0300

    Adição da Biblioteca Krypton para Interfaces Melhores

--- PainelLogin.Designer.cs
+++ PainelLogin.Designer.cs
@@ -28,8 +28,91 @@
         /// </summary>
         private void InitializeComponent()
         {
+            components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(PainelLogin));
+            UserNameInputBox = new Krypton.Toolkit.KryptonTextBox();
+            label1 = new Label();
+            kryptonTextBox1 = new Krypton.Toolkit.KryptonTextBox();
+            label2 = new Label();
+            GoogleLoginButton = new Krypton.Toolkit.KryptonButton();
+            LoginPalette = new Krypton.Toolkit.KryptonCustomPaletteBase(components);
             SuspendLayout();
+            // 
+            // UserNameInputBox
+            // 
+            UserNameInputBox.CornerRoundingRadius = 12F;
+            UserNameInputBox.Location = new Point(12, 177);
+            UserNameInputBox.Name = "UserNameInputBox";
+            UserNameInputBox.Size = new Size(235, 43);
+            UserNameInputBox.StateCommon.Border.DrawBorders = Krypton.Toolkit.PaletteDrawBorders.Top | Krypton.Toolkit.PaletteDrawBorders.Bottom | Krypton.Toolkit.PaletteDrawBorders.Left | Krypton.Toolkit.PaletteDrawBorders.Right;
+            UserNameInputBox.StateCommon.Border.Rounding = 12F;
+            UserNameInputBox.TabIndex = 0;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.BackColor = Color.Transparent;
+            label1.Font = new Font("Segoe UI", 16F, FontStyle.Bold);
+            label1.Location = new Point(12, 144);
+            label1.Name = "label1";
+            label1.Size = new Size(98, 30);
+            label1.TabIndex = 1;
+            label1.Text = "Usuário:";
+            // 
+            // kryptonTextBox1
+            // 
+            kryptonTextBox1.CornerRoundingRadius = 12F;
+            kryptonTextBox1.Location = new Point(12, 256);
+            kryptonTextBox1.Name = "kryptonTextBox1";
+            kryptonTextBox1.Size = new Size(235, 43);
+            kryptonTextBox1.StateCommon.Border.DrawBorders = Krypton.Toolkit.PaletteDrawBorders.Top | Krypton.Toolkit.PaletteDrawBorders.Bottom | Krypton.Toolkit.PaletteDrawBorders.Left | Krypton.Toolkit.PaletteDrawBorders.Right;
+            kryptonTextBox1.StateCommon.Border.Rounding = 12F;
+            kryptonTextBox1.TabIndex = 2;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.BackColor = Color.Transparent;
+            label2.Font = new Font("Segoe UI", 16F, FontStyle.Bold);
+            label2.Location = new Point(12, 223);
+            label2.Name = "label2";
+            label2.Size = new Size(81, 30);
+            label2.TabIndex = 3;
+            label2.Text = "Senha:";
+            // 
+            // GoogleLoginButton
+            // 
+            GoogleLoginButton.CornerRoundingRadius = 50F;
+            GoogleLoginButton.Location = new Point(12, 331);
+            GoogleLoginButton.Name = "GoogleLoginButton";
+            GoogleLoginButton.OverrideDefault.Border.DrawBorders = Krypton.Toolkit.PaletteDrawBorders.None;
+            GoogleLoginButton.OverrideDefault.Content.Draw = Krypton.Toolkit.InheritBool.False;
+            GoogleLoginButton.Palette = LoginPalette;
+            GoogleLoginButton.PaletteMode = Krypton.Toolkit.PaletteMode.Custom;
+            GoogleLoginButton.Size = new Size(235, 60);
+            GoogleLoginButton.StateCommon.Back.Color1 = SystemColors.Window;
+            GoogleLoginButton.StateCommon.Back.Color2 = SystemColors.Window;
+            GoogleLoginButton.StateCommon.Border.Color1 = SystemColors.Window;
+            GoogleLoginButton.StateCommon.Border.Color2 = SystemColors.Window;
+            GoogleLoginButton.StateCommon.Border.DrawBorders = Krypton.Toolkit.PaletteDrawBorders.Top | Krypton.Toolkit.PaletteDrawBorders.Bottom | Krypton.Toolkit.PaletteDrawBorders.Left | Krypton.Toolkit.PaletteDrawBorders.Right;
+            GoogleLoginButton.StateCommon.Border.Rounding = 50F;
+            GoogleLoginButton.StateCommon.Content.LongText.Color1 = Color.Black;
+            GoogleLoginButton.StateCommon.Content.LongText.Color2 = Color.Black;
+            GoogleLoginButton.StateCommon.Content.ShortText.Color1 = Color.Black;
+            GoogleLoginButton.StateCommon.Content.ShortText.Color2 = Color.Black;
+            GoogleLoginButton.StateCommon.Content.ShortText.Font = new Font("Segoe UI", 20F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            GoogleLoginButton.TabIndex = 5;
+            GoogleLoginButton.Values.ImageTransparentColor = SystemColors.Window;
+            GoogleLoginButton.Values.Text = "Login";
+            // 
+            // LoginPalette
+            // 
+            LoginPalette.BaseFont = new Font("Segoe UI", 9F);
+            LoginPalette.BaseFontSize = 9F;
+            LoginPalette.BasePaletteMode = Krypton.Toolkit.PaletteMode.SparkleOrangeLightMode;
+            LoginPalette.BasePaletteType = Krypton.Toolkit.BasePaletteType.Custom;
+            LoginPalette.ThemeName = "";
+            LoginPalette.UseKryptonFileDialogs = true;
             // 
             // PainelLogin
             // 
@@ -37,22 +120,28 @@
             AutoScaleMode = AutoScaleMode.Font;
             BackgroundImage = (Image)resources.GetObject("$this.BackgroundImage");
             BackgroundImageLayout = ImageLayout.Stretch;
-            BlurValues.BlurWhenFocusLost = true;
-            BlurValues.Opacity = 50;
             ClientSize = new Size(800, 560);
             ControlBox = false;
-            CornerRoundingRadius = 20F;
+            Controls.Add(GoogleLoginButton);
+            Controls.Add(label2);
+            Controls.Add(kryptonTextBox1);
+            Controls.Add(label1);
+            Controls.Add(UserNameInputBox);
             FormBorderStyle = FormBorderStyle.None;
             Name = "PainelLogin";
             StartPosition = FormStartPosition.CenterScreen;
-            StateCommon.Border.DrawBorders = Krypton.Toolkit.PaletteDrawBorders.Top | Krypton.Toolkit.PaletteDrawBorders.Bottom | Krypton.Toolkit.PaletteDrawBorders.Left | Krypton.Toolkit.PaletteDrawBorders.Right;
-            StateCommon.Border.Rounding = 20F;
             Text = "PainelLogin";
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
 
-        private Krypton.Toolkit.KryptonButton kryptonButton1;
+        private Krypton.Toolkit.KryptonTextBox UserNameInputBox;
+        private Label label1;
+        private Krypton.Toolkit.KryptonTextBox kryptonTextBox1;
+        private Label label2;
+        private Krypton.Toolkit.KryptonButton GoogleLoginButton;
+        private Krypton.Toolkit.KryptonCustomPaletteBase LoginPalette;
     }
 }
--- Properties/Resources.Designer.cs
+++ Properties/Resources.Designer.cs
@@ -63,6 +63,16 @@ namespace Labs.Properties {
         /// <summary>
         ///   Consulta um recurso localizado do tipo System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap google_logo_9822 {
+            get {
+                object obj = ResourceManager.GetObject("google-logo-9822", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Consulta um recurso localizado do tipo System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap LabsSolutionbackImage {
             get {
                 object obj = ResourceManager.GetObject("LabsSolutionbackImage", resourceCulture);
