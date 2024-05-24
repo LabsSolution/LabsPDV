8d762cf0dfdf7ee579eeb3470e17639a299c796d
IgorIDSM <igordossantos.00@hotmail.com>
2024-05-23 22:37:40 -0300

    Começando a implementação de WPF (Repassando Código)

--- LABS_PDV_MAIN_WPF.cs
+++ LABS_PDV_MAIN_WPF.cs
@@ -8,14 +8,16 @@ using Labs.Janelas.Configuracoes.Dependencias;
 using Labs.Janelas.LabsPDV.Dependencias;
 using Labs.Janelas.LabsPDV;
 using static Labs.LABS_PDV.Modelos;
+using System.Windows;
+using Application = System.Windows.Application;
 //
 namespace Labs
 {
 
-	internal static class LABS_PDV_MAIN
+	internal static class LABS_PDV_MAIN_WPF
 	{
 		//Controle de Instâncias (Endereçamento de memória)
-		private static Dictionary<string, Form> RunningApps = new();
+		private static Dictionary<string, Window> RunningApps = new();
 		//
 		public static string TradeMark = "© Lab Soluções © ";
 		//
@@ -27,14 +29,15 @@ namespace Labs
 		public static string LocalDataBaseConnectionURI = null!;
 		//
 		public static Cliente Cliente { get; private set; } = null!;
-		//
-		//
-		static readonly SVGParser SVGParser = new(); // Desabilitado por enquanto por motivos de performance;
+        //
+        static Application AppInitializer = new();
+        //
+        static readonly SVGParser SVGParser = new(); // Desabilitado por enquanto por motivos de performance;
         /// <summary>
         ///  The main entry point for the application.
         /// </summary>
         [STAThread]
-		static void Main_()
+		static void Main() // Desabilitado Por Enquanto
 		{	
 			//A Criptografia é algo essencial para a segurança dos nossos clientes!
 			// Politica LGPD
@@ -45,18 +48,14 @@ namespace Labs
 			//Descriptografa o nome da Database da empresa
 			if(LabsCripto.Decript("N_Data",out string NDecripted)) { ClientDataBase = NDecripted; }
 			//
-            // To customize application configuration such as set high DPI settings or default font,
-            // see https://aka.ms/applicationconfiguration.
-            ApplicationConfiguration.Initialize();
+			LabsMainAppWPF App = new(); // Altere esse campo para modificar a primeira janela a ser aberta (Utilizar somente para debug)
 			//
-			LabsMainApp App = new(); // Altere esse campo para modificar a primeira janela a ser aberta (Utilizar somente para debug)
-			//
-			App.Resize += OnAppSizeChange;
-            App.Load += OnAppLoad;
+			App.SizeChanged += OnAppSizeChange;
+            App.Loaded += OnAppLoad;
             INIT(App);
         }
         //
-        static void INIT<T>(T App) where T : Form
+        static void INIT<T>(T App) where T : Window
 		{
 			//Inicializamos as dependências obrigatórias
 			//Verifica a pasta config (se não tiver, vai criar uma)
@@ -69,8 +68,8 @@ namespace Labs
 				if(r == DialogResult.Retry) { INIT(App); return; }
 				return; 
 			}
-            // Somente após o sistema verificar tudo é que inicializamos.
-            Application.Run(App);
+			// Somente após o sistema verificar tudo é que inicializamos.
+			AppInitializer.Run(App);
             //
         }
 
@@ -87,71 +86,64 @@ namespace Labs
 
         //EVENTOS//
         //Previne o Cliente de Minimizar o sistema
-        private static void OnAppSizeChange(object? sender, EventArgs e)
+        private static void OnAppSizeChange(object? sender, SizeChangedEventArgs e)
 		{
-			if (sender is Form App)
+			if (sender is Window App)
 			{
-				App.WindowState = FormWindowState.Maximized;
+				App.WindowState = WindowState.Maximized;
 			}
 		}
 		//Método De Retorno Caso a janela seja fechada (Chamada somente nas janelas instanciadas pelo IniciarApp)
-		private static void AppClosed(object? sender, FormClosedEventArgs e)
+		private static void AppClosed(object? sender, EventArgs e)
 		{
-			if (e.CloseReason == CloseReason.UserClosing)
+			if (sender is Window App)
 			{
-				if (sender is Form App)
-				{
-					App.FormClosed -= AppClosed;
-					App.Resize -= OnAppSizeChange;
-                    App.Load -= OnAppLoad;
-                    App.BackgroundImage?.Dispose();
-                    //
-                    LabsMainApp.App.Show();
-				}
+				App.Closed -= AppClosed;
+				App.SizeChanged -= OnAppSizeChange;
+                App.Loaded -= OnAppLoad;
+				//
+				LabsMainAppWPF app = new();
+				app.Show();
 			}
 		}
 		//Método De Retorno caso a janela seja escondida (Também chamada somente nas janelas Instanciadas pelo InicarApp)
 		//Mas nesse caso, somente quando o parametro "Persistente for ativo"
-		private static void AppHidden(object? sender, EventArgs e)
+		private static void AppHidden(object? sender, DependencyPropertyChangedEventArgs e)
 		{
-			if (sender is Form App)
+			if (sender is Window App)
 			{
-				if (!App.Visible)
+				if (!App.IsVisible)
 				{
-					App.VisibleChanged -= AppHidden;
-					App.Resize -= OnAppSizeChange;
-                    App.Load -= OnAppLoad;
+					App.IsVisibleChanged -= AppHidden;
+					App.SizeChanged -= OnAppSizeChange;
+                    App.Loaded -= OnAppLoad;
                     //
-                    LabsMainApp.App.Show();
-				}
+                    LabsMainAppWPF app = new();
+                    app.Show();
+                }
 			}
 		}
 		//
-		private static void DepAppClosed(object? sender, FormClosedEventArgs e)
+		private static void DepAppClosed(object? sender, EventArgs e)
 		{
-			if(e.CloseReason == CloseReason.UserClosing)
+			if (sender is Window App)
 			{
-                if (sender is Form App)
-                {
-                    App.VisibleChanged -= AppHidden;
-                    App.Resize -= OnAppSizeChange;
-                    App.Load -= OnAppLoad;
-                    App.BackgroundImage?.Dispose();
-                    //
-                }
-            }
+			    App.IsVisibleChanged -= AppHidden;
+			    App.SizeChanged -= OnAppSizeChange;
+			    App.Loaded -= OnAppLoad;
+			    //
+			}
         }
 		//
-		private static void DepAppHidden(object? sender, EventArgs e)
+		private static void DepAppHidden(object? sender, DependencyPropertyChangedEventArgs e)
 		{
-            if (sender is Form App)
+            if (sender is Window App)
             {
-                if (!App.Visible)
+                if (!App.IsVisible)
                 {
-                    App.VisibleChanged -= AppHidden;
-                    App.Resize -= OnAppSizeChange;
-					App.Load -= OnAppLoad;
-					App.Dispose();
+                    App.IsVisibleChanged -= AppHidden;
+                    App.SizeChanged -= OnAppSizeChange;
+					App.Loaded -= OnAppLoad;
                 }
             }
         }
@@ -161,11 +153,11 @@ namespace Labs
 		/// </summary>
 		/// <typeparam name="T">Dependencia a ser Iniciada</typeparam>
 		/// <param name="SempreNoTopo">Mostrar Janela sempre no topo ou não</param>
-		public static T IniciarDependencia<T>(Action<T> config = null!, bool SempreNoTopo = true, bool BackgroundImage = false) where T : Form, new()
+		public static T IniciarDependencia<T>(Action<T> config = null!, bool SempreNoTopo = true, bool BackgroundImage = false) where T : Window, new()
 		{
 			T? App;
 			// Verifica se a aplicação já está rodando
-			if (RunningApps.TryGetValue(typeof(T).Name, out Form? existingApp))
+			if (RunningApps.TryGetValue(typeof(T).Name, out Window? existingApp))
 			{
 				App = existingApp as T;
 			}
@@ -177,7 +169,7 @@ namespace Labs
 			}
 
 			// Verifica se a aplicação está descartada
-			if (App.IsDisposed)
+			if (App.IsActive)
 			{
 				// Cria uma nova instância da aplicação se a antiga estiver descartada
 				App = new T();
@@ -188,17 +180,17 @@ namespace Labs
 			//
             if (SempreNoTopo) 
 			{ 
-				if (BackgroundImage == true)  { App.Load += OnAppLoad; } 
+				if (BackgroundImage == true)  { App.Loaded += OnAppLoad; } 
 				//
-				App.FormClosed += DepAppClosed; 
-				App.VisibleChanged += DepAppHidden; 
+				App.Closed += DepAppClosed; 
+				App.IsVisibleChanged += DepAppHidden; 
 				App.ShowDialog(); 
 				//
 				return App; 
 			}
 			//Retornamos o App
-			if (BackgroundImage == true) { App.Load += OnAppLoad; }
-			App.FormClosed += DepAppClosed;
+			if (BackgroundImage == true) { App.Loaded += OnAppLoad; }
+			App.Closed += DepAppClosed;
 			App.Show();
 			return App;
 		}
@@ -209,11 +201,11 @@ namespace Labs
 		/// <typeparam name="T">Tipo de Janela que será iniciado</typeparam>
 		/// <param name="Persistente">Define se a janela permanece carregada até ser forçado seu fechamento ou não</param>
 		/// <param name="BackgroundImage">Define se a janela Carrega a imagem de Background ou não, padrão = true</param>
-		public static T IniciarApp<T>(bool Persistente = false, bool BackgroundImage = false) where T : Form, new()
+		public static T IniciarApp<T>(bool Persistente = false, bool BackgroundImage = false) where T : Window, new()
 		{
 			T? App;
 			// Verifica se a aplicação já está rodando
-			if (RunningApps.TryGetValue(typeof(T).Name, out Form? existingApp))
+			if (RunningApps.TryGetValue(typeof(T).Name, out Window? existingApp))
 			{
 				App = existingApp as T;
 			}
@@ -225,7 +217,7 @@ namespace Labs
 			}
 
 			// Verifica se a aplicação está descartada
-			if (App.IsDisposed)
+			if (App.IsActive)
 			{
 				// Cria uma nova instância da aplicação se a antiga estiver descartada
 				App = new T();
@@ -239,10 +231,10 @@ namespace Labs
 			//
 			// Aqui atrelamos os dois eventos porque em algum momento a janela será realmente fechada;
 			//
-			if (Persistente) { App.VisibleChanged += AppHidden;  }
-			if (BackgroundImage == true) { App.Load += OnAppLoad; }
-			App.Resize += OnAppSizeChange; // O evento de SizeChange é global para qualquer janela;
-            App.FormClosed += AppClosed; // Global
+			if (Persistente) { App.IsVisibleChanged += AppHidden;  }
+			if (BackgroundImage == true) { App.Loaded += OnAppLoad; }
+			App.SizeChanged += OnAppSizeChange; // O evento de SizeChange é global para qualquer janela;
+            App.Closed += AppClosed; // Global
             // Mostra a aplicação
             App.Show();
             return App; // Após isso tudo, retornamos a janela
---
+++ LabsMainAppWPF.xaml.cs
@@ -0,0 +1,110 @@
+﻿using Labs.Janelas.Configuracoes;
+using Labs.Janelas.LabsEstoque;
+using Labs.Janelas.LabsPDV;
+using Labs.LABS_PDV;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Labs
+{
+    /// <summary>
+    /// Lógica interna para Window1.xaml
+    /// </summary>
+    public partial class LabsMainAppWPF : Window
+    {
+        //
+        public static int QMDP { get; private set; } = -1;
+        //
+        // Esses campos serão setados ao carregar as impressoras configuradas
+        public static string ImpressoraTermica { get; private set; } = null!;
+        public static string ImpressoraA4 { get; private set; } = null!;
+        /// <summary>
+        /// Habilitado Somente Caso Algum Erro Crítico de Inicialização for Detectado
+        /// </summary>
+        public static bool ModoSegurança { get; private set; } = false;
+        //
+        public LabsMainAppWPF()
+        {
+            InitializeComponent();
+            LoadConfigs();
+            //TEMPORÁRIO
+            LABS_PDV_MAIN.LabsCloudDataBaseConnectionURI = LABS_PDV_MAIN_WPF.LabsCloudDataBaseConnectionURI;
+            LABS_PDV_MAIN.CloudDataBaseConnectionURI = LABS_PDV_MAIN_WPF.CloudDataBaseConnectionURI;
+            LABS_PDV_MAIN.LocalDataBaseConnectionURI = LABS_PDV_MAIN_WPF.LocalDataBaseConnectionURI;
+            LABS_PDV_MAIN.ClientDataBase = LABS_PDV_MAIN_WPF.ClientDataBase;
+        }
+        //
+        bool VerifyDataBases()
+        {
+            bool canProceed = true;
+            if (!CloudDataBase.CheckDataBaseConnection(out bool LocalOK, out bool CloudOK, out bool LabsCloudOK))
+            {
+                Modais.MostrarErro("ERRO CRÍTICO\nNão Foi Possivel Estabelecer Conexão com os Bancos de Dados!");
+                LabsPDVButton.IsEnabled = false;
+                LabsEstoqueButton.IsEnabled = false;
+                canProceed = false;
+            }
+            return canProceed;
+        }
+        //
+        private void LoadConfigs()
+        {
+            //Aqui Lemos as Configs do App.Config;
+            if (Utils.TryParseToInt(LabsConfigs.GetConfigValue("QMDP"), out int value))
+            {
+                QMDP = value;
+            }
+            //Aqui Puxamos as Impressoras Configuradas Para uso do sistema (Configuradas pelo painel de Configs);
+            // o valor vai ser retornado como Null caso não tenha nenhuma impressora configurada!
+            ImpressoraTermica = LabsConfigs.GetConfigValue("ImpressoraTermica");
+            ImpressoraA4 = LabsConfigs.GetConfigValue("ImpressoraA4");
+            //
+        }
+        //
+        static async void VerificacoesPreventivas()
+        {
+            await GerenciadorPDV.VerificarEstoque(QMDP);
+        }
+        //
+        private void OnLabsEstoqueClick(object sender, RoutedEventArgs e)
+        {
+            LABS_PDV_MAIN.IniciarDependencia<Labs_Estoque>();
+        }
+
+        private void OnLabsPDVClick(object sender, RoutedEventArgs e)
+        {
+            LABS_PDV_MAIN.IniciarDependencia<LabsPDV>();
+        }
+
+        private void OnLabsConfigClick(object sender, RoutedEventArgs e)
+        {
+            LABS_PDV_MAIN.IniciarDependencia<LabsConfig>();
+        }
+
+        private void OnLabsSairClick(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void OnLabsMainAppLoad(object sender, RoutedEventArgs e)
+        {
+            //Quando forem repassadas para wpf reativar
+
+            //if (!VerifyDataBases()) { ModoSegurança = true; Modais.MostrarAviso("MODO DE SEGURANÇA HABILITADO!\nPara Sair Desse Modo, Os Conflitos Devem ser Resolvidos\ne Logo Após o Sistema Deve Ser Reiniciado!"); return; }
+            //VerificacoesPreventivas();
+        }
+        //
+
+    }
+}
